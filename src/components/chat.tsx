2a2a21ea252f548c6f226044f14b68f3432e22e7
kualta <contact@kualta.dev>
2025-05-01 17:59:41 +0400

    add user avatar

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -15,6 +15,7 @@ import { Chat } from "@/lib/db/schema";
 import { ID } from "jazz-tools";
 import { redirect, useParams } from "next/navigation";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 interface ChatViewProps {
   chatID?: ID<Chat>;
@@ -23,7 +24,9 @@ interface ChatViewProps {
 export default function ChatView({ chatID }: ChatViewProps) {
   const params = useParams();
   const isAuthenticated = useIsAuthenticated();
+  const { me } = useAccount()
   const currentChatID = chatID || (params?.id as ID<Chat>);
+  const avatar = me?.profile?.picture;
 
   if (!isAuthenticated) {
     redirect("/");
@@ -38,7 +41,13 @@ export default function ChatView({ chatID }: ChatViewProps) {
 
             {currentChatID && <MessagesArea chatID={currentChatID} />}
 
-            <div className="p-3 pb-4 mt-auto absolute bottom-0 left-0 right-0 z-10 bg-transparent">
+            <div className="p-4 pr-5 mt-auto gap-4 flex flex-row items-center absolute bottom-0 left-0 right-0 z-10 bg-transparent">
+              {avatar && <Avatar className="w-10 h-10" >
+                <AvatarImage src={avatar} />
+                <AvatarFallback>
+                  {me?.profile?.name?.slice(0, 1)}
+                </AvatarFallback>
+              </Avatar>}
               {currentChatID && <MessageInput chatID={currentChatID} />}
             </div>
           </div>
